869441ee8e21896a5065a785c79680060c60b967
Guilherme Castello <guilherme.brasolin@outlook.com>
2022-07-15 02:40:44 -0300

    Changing navbar logo and colors when scroll down

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ window.addEventListener('scroll', navbarSticky);
 
 //Variable Utils
 const navbar = document.querySelector('.navbar-container');
-
+const logo = document.querySelector('.logo-img');
 
 function navbarSticky() {
     navbar.classList.toggle('sticky', window.scrollY > 0);
+    if (window.scrollY > 0) {
+        logo.src = '/media/images/logo-sticky.png';
+        logo.style = 'padding-top: 5px;'
+    } else {
+        logo.src = '/media/images/logo.png';
+    }
 }
