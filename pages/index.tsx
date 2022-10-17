59460407dc31a88b6f68e4a6cc52859c04499263
Guilherme Castello <guilherme.brasolin@outlook.com>
2022-10-17 04:24:07 -0300

    adding lines and computer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import type { NextPage } from 'next'
 import Navbar from '../components/Navbar'
-import logo from '../assets/logo.png'
+import logo from '../assets/logomarca-branco.png'
 import wave from '../assets/svg/wave.svg'
+import Lines from '../components/Lines'
+import Computer from '../components/Computer'
 
 const Home: NextPage = () => {
+  console.log(wave);
+
   return (
     <div>
-      <section className='bg-[#352575] w-full h-screen'>
+      <section className='bg-[#352575] w-full h-screen relative'>
         <Navbar logo={logo}>
           <span>Home</span>
           <span>Sobre</span>
@@ -14,8 +18,13 @@ const Home: NextPage = () => {
           <span>Portifólio</span>
           <span>Contato</span>
         </Navbar>
-        <div style={{ height: 'calc(100% - 56px)' }}></div>
+        <div style={{ height: 'calc(100% - 56px)' }} className="relative">
+          <Computer></Computer>
+          <Lines></Lines>
+          <img src={wave.src} alt="wave" className='absolute bottom-0' />
+        </div>
       </section>
+      <section className='bg-[#F3F4F5] w-full h-screen relative'></section>
     </div>
   )
 }
