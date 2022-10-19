ed4f56e69084afb0313aa90775777cb27cefd1d7
Guilherme Castello <guilherme.brasolin@outlook.com>
2022-10-19 06:10:59 -0300

    fixing autowrite component

--- components/AutoWrite.tsx
+++ components/AutoWrite.tsx
@@ -1,22 +1,26 @@
 
+import { CSSProperties } from 'react';
 import Typed from 'react-typed';
 
 interface AutoWriteProps {
     text: Array<string>,
-    loop: boolean,
-    showCursor: boolean,
+    loop?: boolean,
+    showCursor?: boolean,
     className?: string,
-    typeSpeed:number
+    typeSpeed: number,
+    delayed?: boolean,
+    style?: CSSProperties
 }
 
-export default function AutoWrite(props: AutoWriteProps) {
-    return (
-        <Typed
-            strings={props.text}
-            typeSpeed={props.typeSpeed}
-            loop={props.loop}
-            className={`text-3xl md:text-5xl font-[HermitBold] ${props.className}`}
-            showCursor={props.showCursor}
-        />
-    )
+export default function AutoWrite(props: AutoWriteProps) {    
+        return (
+            <Typed
+                strings={props.text}
+                typeSpeed={props.typeSpeed}
+                loop={props.loop}
+                className={`text-3xl md:text-5xl font-[HermitBold] ${props.className}`}
+                showCursor={props.showCursor}
+                style={props.style}
+            />
+        )
 }
