73b22213a954ebc979521f4e366f18b4fad0eeb1
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-10-21 11:47:28 -0300

    adding floating icon to index page, second and third section

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,26 +5,36 @@ import logoMobile from '../assets/logo.png'
 import MobileNavbar from '../components/MobileNavbar'
 import FirstSectionBody from '../components/FirstSectionBody'
 import Script from 'next/script'
+import SecondSectionBody from '../components/SecondSectionBody'
+import FloatingIcon from '../components/FloatingIcon'
+import ThirdSectionBody from '../components/ThirdSectionBody'
 
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <Script src='https://kit.fontawesome.com/796432032e.js'></Script>
-      <section id="home" className='bg-[#352575] w-full h-screen relative overflow-hidden'>
-        <Navbar logo={logo}>
-          <span>Home</span>
-          <span>Sobre</span>
-          <span>Portfólio</span>
-          <span>Contato</span>
-        </Navbar>
-        <MobileNavbar logo={logoMobile}>
-          
-        </MobileNavbar>
-        <FirstSectionBody></FirstSectionBody>
-      </section>
-      <section className='bg-[#F3F4F5] w-full h-screen relative'></section>
-    </div>
+    <>
+      <FloatingIcon></FloatingIcon>
+      <div>
+        <Script src='https://kit.fontawesome.com/796432032e.js'></Script>
+        <section id="home" className='bg-[#352575] w-full h-screen relative overflow-hidden'>
+          <Navbar logo={logo}>
+            <span>Home</span>
+            <span>Sobre</span>
+            <span>Portfólio</span>
+            <span>Contato</span>
+          </Navbar>
+          <MobileNavbar logo={logoMobile}>
+          </MobileNavbar>
+          <FirstSectionBody></FirstSectionBody>
+        </section>
+        <section id="sobre" className='bg-[#F3F4F5] w-full h-screen relative'>
+          <SecondSectionBody></SecondSectionBody>
+        </section>
+        <section id="portfolio" className='bg-[#352575] w-full h-screen relative'>
+          <ThirdSectionBody></ThirdSectionBody>
+        </section>
+      </div>
+    </>
   )
 }
 
