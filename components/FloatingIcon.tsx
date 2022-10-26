748b23b35739326cea0a707066c6da76b1106591
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-10-26 04:28:30 -0300

    fixing the size and position of floating icon area

--- components/FloatingIcon.tsx
+++ components/FloatingIcon.tsx
@@ -1,7 +1,6 @@
 export default function FloatingIcon() {
     return (
-        <div className='fixed right-0 w-2/12 h-full z-[60] upIconContainer'>
-
+        <div className='fixed right-0 bottom-0 w-2/12 h-[calc(100%_-_20%)] z-[60] upIconContainer'>
             <div className="w-12 h-12 bg-[#352575] fixed rounded-full right-[-5rem] bottom-8 up-icon transition-all duration-300 flex flex-row 
             justify-center items-center cursor-pointer">
                 <a href="#home">
